8526a514b2472e94ab91d20b5f47cafd94a3fe0c
kmw4097 <kmw4097@naver.com>
2023-06-07 08:19:50 +0900

    update LIIF

---
+++ LIIF/demo.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+from PIL import Image
+
+import torch
+from torchvision import transforms
+
+import models
+from utils import make_coord
+from test import batched_predict
+
+
+def run(img_path, model_path, output_path, resolution_width, resolution_height):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input', default= img_path)
+    parser.add_argument('--model', default = model_path)
+    parser.add_argument('--resolution', default = (resolution_height,resolution_width))
+    parser.add_argument('--output', default=output_path)
+    parser.add_argument('--gpu', default='0')
+    args = parser.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    img = transforms.ToTensor()(Image.open(args.input).convert('RGB'))
+
+    model = models.make(torch.load(args.model, map_location=torch.device('cpu'))['model'], load_sd=True)#.cuda()
+    
+    h, w = int(args.resolution[0]), int(args.resolution[1])
+    #h, w = list(map(int, args.resolution.split(',')))
+    coord = make_coord((h, w))#.cuda()
+    cell = torch.ones_like(coord)
+    cell[:, 0] *= 2 / h
+    cell[:, 1] *= 2 / w
+    pred = batched_predict(model, ((img - 0.5) / 0.5).unsqueeze(0),
+        coord.unsqueeze(0), cell.unsqueeze(0), bsize=30000)[0]
+    pred = (pred * 0.5 + 0.5).clamp(0, 1).view(h, w, 3).permute(2, 0, 1).cpu()
+    transforms.ToPILImage()(pred).save(args.output)
---
+++ LIIF/module_super_resolution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import torch
+import os
+from PIL import Image
+import numpy as np
+import demo
+
+# for test
+# img1_path = 'LIIF/images/image1.jpg'
+# img2_path = 'LIIF/images/image2.jpg'
+
+def make_high_resolution(img_path, resolution_width, resolution_height):
+    demo.run(img_path = img_path, output_path = 'LIIF/outputs/output1.jpg', model_path = 'LIIF/edsr-baseline-liif.pth', resolution_width = resolution_width, resolution_height = resolution_height)
+
+# make_high_resolution(img_path = img2_path, resolution_width = 300, resolution_height = 600)
