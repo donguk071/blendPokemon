cd00c04e0a8ee1964c4cacb08509ac21b710f11b
donguk <dragon071@naver.com>
2023-06-07 00:19:29 +0900

    to func

--- MAT/module_inpainting.py
+++ MAT/module_inpainting.py
@@ -1,20 +1,26 @@
 from generate_image import generate_images 
 import os
 
-network_pkl = './MAT/pretrained/Places_512.pkl'
-dpath = './MAT/test_sets/CelebA-HQ/images'
-mpath = './MAT/test_sets/CelebA-HQ/masks'
-outdir = './MAT/mytest'
-# 함수 호출
-generate_images(
-    network_pkl=network_pkl,
-    resolution =512,
-    truncation_psi =1,
-    noise_mode = "const",
-    dpath=dpath,
-    mpath=mpath,
-    outdir=outdir
-)
+
+def generate_images_ours(network_pkl = './MAT/pretrained/Places_512.pkl'
+                         , dpath = './MAT/test_sets/CelebA-HQ/images'
+                         ,mpath = './MAT/test_sets/CelebA-HQ/masks'
+                         ,outdir = './MAT/mytest'):
+    
+    # 함수 호출
+    generate_images(
+        network_pkl=network_pkl,
+        resolution =512,
+        truncation_psi =1,
+        noise_mode = "const",
+        dpath=dpath,
+        mpath=mpath,
+        outdir=outdir
+    )
+    
+generate_images_ours()
+    
+    
 # generate_images(
 #     network_pkl='C:/Users/drago/university/23.1 semester/DL/inpainting/MAT/pretrained/Places_512_FullData.pkl',
 #     dpath='test_sets\CelebA-HQ\images',
