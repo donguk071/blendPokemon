5e0d31df0444d45b8aa82d75aac026870062b19b
kmw4097 <kmw4097@naver.com>
2023-06-07 08:36:47 +0900

    update LIIF

--- LIIF/module_super_resolution.py
+++ LIIF/module_super_resolution.py
@@ -6,16 +6,16 @@ import numpy as np
 import demo
 
 # for test
-img1_path = 'LIIF/images/image1.jpg'
-img2_path = 'LIIF/images/image2.jpg'
-output_path = 'LIIF/outputs/output1.jpg'
+# img1_path = 'LIIF/images/image1.jpg'
+# img2_path = 'LIIF/images/image2.jpg'
+# output_path = 'LIIF/outputs/output1.jpg'
 
 def make_high_resolution(img_path, output_path, resolution_width, resolution_height):
-    
+
     demo.run(img_path = img_path, 
              output_path = output_path, 
              model_path = 'LIIF/edsr-baseline-liif.pth', 
              resolution_width = resolution_width, 
              resolution_height = resolution_height)
 
-make_high_resolution(img_path = img2_path, resolution_width = 300, resolution_height = 600)
+# make_high_resolution(img_path = img2_path, resolution_width = 300, resolution_height = 600)
