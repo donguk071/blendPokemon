4fead936f96a634cf0b174e419a8873dafdaff26
Haneul Kim <haneul.kim.dev@gmail.com>
2023-06-10 02:32:49 +0900

    Add generate_mask function

--- SAM/generate_mask.py
+++ SAM/generate_mask.py
@@ -38,6 +38,74 @@ parser.add_argument(
 parser.add_argument("--device", type=str, default="cuda", help="The device to run generation on.")
 
 
+def generate_mask(
+    image_path: str,
+    sam_checkpoint: str = "../SAM/sam_vit_h_4b8939.pth",
+    model_type: str = "vit_h",
+    device: str = "cuda",
+) -> None:
+    print("Loading model...")
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+
+    output = os.path.join(os.path.dirname(image_path), "outputs")
+    os.makedirs(output, exist_ok=True)
+
+    print(f"Processing '{image_path}'...")
+    image = cv.imread(image_path)
+
+    predictor.set_image(cv.cvtColor(image, cv.COLOR_BGR2RGB))
+
+    base = os.path.basename(image_path)
+    base = os.path.splitext(base)[0]
+    save_base = os.path.join(output, base)
+
+    def mouse_callback(event, x, y, flags, param):
+        global mask, mask_inv, img_fg, img_bg
+
+        if event == cv.EVENT_LBUTTONDOWN:
+            input_point = np.array([[x, y]])
+            input_label = np.array([1])
+
+            masks, _, _ = predictor.predict(
+                point_coords=input_point,
+                point_labels=input_label,
+                multimask_output=True,
+            )
+
+            mask = masks[-1]
+            h, w = mask.shape[-2:]
+            mask = mask.reshape(h, w, 1).astype(np.uint8) * 255
+            mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+            mask_inv = cv.bitwise_not(mask)
+
+            img_fg = cv.bitwise_and(image, mask)
+            img_bg = cv.bitwise_and(image, mask_inv)
+
+            img_result = cv.vconcat([cv.hconcat([mask, mask_inv]), cv.hconcat([img_fg, img_bg])])
+            cv.imshow("res", img_result)
+
+    winname = "image"
+    cv.namedWindow(winname)
+    cv.setMouseCallback(winname, mouse_callback)
+
+    while True:
+        cv.imshow(winname, image)
+        k = cv.waitKey(0)
+        if k == 27:  # wait for ESC key to exit
+            break
+        elif k == ord("s"):  # wait for 's' key to save and exit
+            os.makedirs(save_base, exist_ok=True)
+            cv.imwrite(os.path.join(save_base, "mask.png"), mask)
+            cv.imwrite(os.path.join(save_base, "mask_inv.png"), mask_inv)
+            cv.imwrite(os.path.join(save_base, "img_fg.png"), img_fg)
+            cv.imwrite(os.path.join(save_base, "img_bg.png"), img_bg)
+            break
+    cv.destroyAllWindows()
+    print("Done!")
+
+
 def main(args: argparse.Namespace) -> None:
     print("Loading model...")
     sam = sam_model_registry[args.model_type](checkpoint=args.checkpoint)
